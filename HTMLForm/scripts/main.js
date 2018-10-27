988805376030cc89e5005f399a3616da6417e092
Shelby Neubeck <neubeckshelby@gmail.com>
2018-10-27 12:40:50 -0500

    Assessment 2 submit

--- HTMLForm/scripts/main.js
+++ HTMLForm/scripts/main.js
@@ -3,6 +3,33 @@ $(function(){
 
 
     function submit(){
+        $("#success").html("");
+        var errorsEl = $('#errors');
+        var errors = '';
+        var firstName = $('#firstName');
+        var firstNameValue = firstName.val();
 
+
+        var email = $('#email');
+        var emailValue = email.val();
+
+        if (firstNameValue === '') {
+            errors += 'First name can not be empty.</br>';
+            firstName.addClass('invalid');
+        }
+
+        if (emailValue === '') {
+            email.addClass('invalid');
+            errors += 'Email Address can not be empty.</br>';
+        } else if (emailValue.indexOf('@') === -1) {
+            email.addClass('invalid');
+            errors += emailValue + ' is not a valid email addresss</br>';
+        }
+        if(errors != '') {
+            errorsEl.html(errors);
+        } else {
+            $("#success").html("Great success!");
+        }
     }
 });
+
